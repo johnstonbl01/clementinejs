e622520181f17dde9c1f052ccc68bb06924fc942
Blake Johnston <johnstonbl01@gmail.com>
2015-05-14 16:52:56 -0500

    remove redundant directories in gh-pages branch

--- app/models/comment.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Comment = new Schema({
-	email: String,
-	comments: String
-});
-
-module.exports = mongoose.model('Comment', Comment);
